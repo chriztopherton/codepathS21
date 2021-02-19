b3f5e977a415d1ca0553eb4796121f8284c5376f
chriztopherton <christopherton1998@gmail.com>
2021-02-18 19:41:15 -0800

    unit 2 upload

---
+++ flixster/flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Christopher Ton on 2/17/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
